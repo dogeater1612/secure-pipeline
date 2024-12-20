3fc2835a1428101187a059df1942466f674def06
Singyu <Singyu.Liu@student.ap.be>
2024-12-20 17:25:13 +0100

    Codeql vulnerability

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,12 @@
+import subprocess
+
+
 print("Hello world!")
 password = "mysecretpassword"  
-eval(input("Type something: "))  
+eval(input("Type something: "))
+
+
+
+user_input = input("Enter something")
+
+subprocess.run(user_input,shell=True)
